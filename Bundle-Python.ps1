d86e92feac8cd955781c56f1bfbb19f44b729f69
Juraj Michálek <juraj.michalek@espressif.com>
2020-12-14 11:41:43 +0100

    use relative location for chocolatey

--- Bundle-Python.ps1
+++ Bundle-Python.ps1
@@ -8,16 +8,16 @@ Remove-Item "python\python38._pth"
 Remove-Item "python.zip"
 
 # Virtualenv layer
-Invoke-WebRequest -Uri "https://bootstrap.pypa.io/get-pip.py" -Out "C:\Temp\get-pip.py"
+Invoke-WebRequest -Uri "https://bootstrap.pypa.io/get-pip.py" -Out "get-pip.py"
 .\python\python.exe "get-pip.py"
 .\python\python.exe -m pip install virtualenv
 
 # Virtualenv helper files must be extracted from main python.
 # Embedded version does not contain working copy for virtualenv.
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install python3 -Y --version=3.8.6
+choco install --no-progress --install-directory="${pwd}\temp-python3" python3 -Y --version=3.8.6
 mkdir python\Lib\venv\scripts\nt
-Copy-Item C:\Python38\Lib\venv\scripts\nt\*.exe python\Lib\venv\scripts\nt
+Copy-Item temp-python3\Lib\venv\scripts\nt\*.exe python\Lib\venv\scripts\nt
 
 # Create final zip
 Compress-Archive -Path "python" -DestinationPath "python.zip"
