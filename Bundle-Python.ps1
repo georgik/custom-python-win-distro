ea8d7396ab7ababb13741e57fae69941f0bfece7
Juraj Michálek <juraj.michalek@espressif.com>
2021-01-12 14:34:54 +0100

    zip archive

--- Bundle-Python.ps1
+++ Bundle-Python.ps1
@@ -43,4 +43,4 @@ Copy-Item ${PythonVenvScripts}\python.exe ${PythonDirectory}\Lib\venv\scripts\nt
 Copy-Item ${PythonVenvScripts}\pythonw.exe ${PythonDirectory}\Lib\venv\scripts\nt
 
 # Create final zip - GitHub performs compression of artifacts automatically
-#Compress-Archive -Path "python" -DestinationPath "python.zip"
+Compress-Archive -Path "python\*" -DestinationPath "python.zip"
